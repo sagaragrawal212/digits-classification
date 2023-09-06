6008762513bff60f86c025736a7107b5327233ae
root <root@LAPTOP-EAUUV3I2.localdomain>
2023-09-06 21:43:08 +0530

    adds hyper parameter tuning

--- utils.py
+++ utils.py
@@ -85,48 +85,51 @@ def predict_and_eval(model , X_test , y_test ) :
     # 
     predicted = model.predict(X_test)
 
-    ###############################################################################
-    # Below we visualize the first 4 test samples and show their predicted
-    # digit value in the title.
+    # ###############################################################################
+    # # Below we visualize the first 4 test samples and show their predicted
+    # # digit value in the title.
 
-    _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-    for ax, image, prediction in zip(axes, X_test, predicted):
-        ax.set_axis_off()
-        image = image.reshape(8, 8)
-        ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-        ax.set_title(f"Prediction: {prediction}")
+    # _, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+    # for ax, image, prediction in zip(axes, X_test, predicted):
+    #     ax.set_axis_off()
+    #     image = image.reshape(8, 8)
+    #     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
+    #     ax.set_title(f"Prediction: {prediction}")
 
-    ###############################################################################
-    # :func:`~sklearn.metrics.classification_report` builds a text report showing
-    # the main classification metrics.
+    # ###############################################################################
+    # # :func:`~sklearn.metrics.classification_report` builds a text report showing
+    # # the main classification metrics.
 
-    print(
-        f"Classification report for classifier {model}:\n"
-        f"{metrics.classification_report(y_test, predicted)}\n"
-    )
+    # print(
+    #     f"Classification report for classifier {model}:\n"
+    #     f"{metrics.classification_report(y_test, predicted)}\n"
+    # )
 
 
-    disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-    disp.figure_.suptitle("Confusion Matrix")
-    print(f"Confusion matrix:\n{disp.confusion_matrix}")
+    # disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
+    # disp.figure_.suptitle("Confusion Matrix")
+    # print(f"Confusion matrix:\n{disp.confusion_matrix}")
 
-    plt.show()
+    # plt.show()
 
-    # The ground truth and predicted lists
-    y_true = []
-    y_pred = []
-    cm = disp.confusion_matrix
+    # # The ground truth and predicted lists
+    # y_true = []
+    # y_pred = []
+    # cm = disp.confusion_matrix
 
-    # For each cell in the confusion matrix, add the corresponding ground truths
-    # and predictions to the lists
-    for gt in range(len(cm)):
-        for pred in range(len(cm)):
-            y_true += [gt] * cm[gt][pred]
-            y_pred += [pred] * cm[gt][pred]
+    # # For each cell in the confusion matrix, add the corresponding ground truths
+    # # and predictions to the lists
+    # for gt in range(len(cm)):
+    #     for pred in range(len(cm)):
+    #         y_true += [gt] * cm[gt][pred]
+    #         y_pred += [pred] * cm[gt][pred]
 
-    print(
-        "Classification report rebuilt from confusion matrix:\n"
-        f"{metrics.classification_report(y_true, y_pred)}\n"
-    )
+    # print(
+    #     "Classification report rebuilt from confusion matrix:\n"
+    #     f"{metrics.classification_report(y_true, y_pred)}\n"
+    # )
 
-    return y_pred
+    test_acc = metrics.accuracy_score(y_test,predicted)
+    
+    # print("Test Accuracy : ",test_acc)
+    return test_acc 
