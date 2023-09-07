2f7fb8a55e9c266f686433c5320ff210c2cd1414
root <root@LAPTOP-EAUUV3I2.localdomain>
2023-09-07 08:59:38 +0530

    implements non nested parameter searcihing and also iterates over different test dev sizes

--- exp.py
+++ exp.py
@@ -10,7 +10,7 @@ hand-written digits, from 0-9.
 
 # Standard scientific Python imports
 import matplotlib.pyplot as plt
-
+import itertools
 # Import datasets, classifiers and performance metrics
 import pdb
 from utils import *
@@ -27,37 +27,40 @@ for ax, image, label in zip(axes, X, y):
     ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
     ax.set_title("Training: %i" % label)
 
-# Data Splitting -- to create train and test sets
-X_train, X_test,X_dev, y_train, y_test,y_dev = split_data(X , y,test_size = 0.3,dev_size = 0.2)
-print(f"Train data size : {len(X_train)}")
-print(f"Dev data size : {len(X_test)}")
-print(f"Val data size : {len(X_dev)}")
 
-# Data prepoocessing
-X_train = preprocess_data(X_train)
-X_test = preprocess_data(X_test)
-X_dev = preprocess_data(X_dev)
+print("Iterating for different test and dev size : ")
+dev_splits = [0.1,0.2,0.3]
+test_splits =[0.1,0.2,0.3]
+test_dev_list = [dev_splits,test_splits]
+all_test_dev_combination = all_param_comb_list = list(itertools.product(*test_dev_list)) 
 
-#HYPER PARAMETER TUNING 
-best_acc_so_far = -1
-best_model = None
-for cur_gamma in gamma_ranges :
-    for cur_C in C_ranges :
-       
-        cur_model  = train_model(X_train,y_train, {'gamma' : cur_gamma , 'C' : cur_C}, model_type = 'svm')
-        cur_accuracy = predict_and_eval(cur_model,X_dev,y_dev)
+for dev_size , test_size in all_test_dev_combination :
+    # Data Splitting -- to create train and test sets
+    X_train, X_test,X_dev, y_train, y_test,y_dev = split_data(X , y,test_size = test_size,dev_size = dev_size)
+   
+    # Data prepoocessing
+    X_train = preprocess_data(X_train)
+    X_test = preprocess_data(X_test)
+    X_dev = preprocess_data(X_dev)
 
-        if cur_accuracy > best_acc_so_far :
-            print("New best accuracy : ",cur_accuracy)
-            best_acc_so_far = cur_accuracy
-            optimal_gamma = cur_gamma
-            optimal_C = cur_C
-            best_model = cur_model
 
-print(f"Optimal para gamma = {optimal_gamma} and C = {optimal_C}")
-# Model training : Create a classifier: a support vector classifier
-# model = train_model(X_train, y_train , {'gamma' : optimal_gamma , 'C' : optimal_C} , model_type = 'svm')
+    #HYPER PARAMETER TUNING 
+    gamma_ranges = [0.001,0.01,0.1,1,10,100]
+    C_ranges = [0.1 ,1,2,5,10]
 
-# 6. Getting the predictions on test set
-test_acc = predict_and_eval(best_model , X_test , y_test ) 
-print("Test Accuracy : ",test_acc)
+    all_param_list = [gamma_ranges,C_ranges]
+    all_param_comb_list = list(itertools.product(*all_param_list))
+    list_of_all_param_combination = [{'gamma' : g , 'C' : C } for g , C in all_param_comb_list]
+    optimal_params , best_model , best_acc_so_far = tune_hparams(X_train,y_train,X_dev,y_dev,list_of_all_param_combination) 
+
+    # print(f"Optimal para gamma = {optimal_params} ")
+    # Model training : Create a classifier: a support vector classifier
+    # model = train_model(X_train, y_train , {'gamma' : optimal_gamma , 'C' : optimal_C} , model_type = 'svm')
+
+    # 6. Getting the predictions on test set
+    train_acc = predict_and_eval(best_model , X_train , y_train ) 
+    dev_acc = predict_and_eval(best_model , X_dev , y_dev ) 
+    test_acc = predict_and_eval(best_model , X_test , y_test )
+    print() 
+    print(f"test_size = {test_size} ,  dev_size = {dev_size} ,  train_size  = {1 - (test_size + dev_size)} train_acc = {train_acc} dev_acc = {dev_acc}  test_acc == {test_acc} ")
+    print(f"Optimal Parameters : {optimal_params}")
